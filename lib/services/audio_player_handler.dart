d43470f6cbd7063b83989e2c6c8a20cb8d687ad7
GL_ScherrerS <svetlana.scherrer@h-ka.de>
2026-01-04 22:21:57 +0100

    correction for 2. pause() in notifications trial #9

--- lib/services/audio_player_handler.dart
+++ lib/services/audio_player_handler.dart
@@ -2,6 +2,7 @@ import 'package:audio_service/audio_service.dart';
 import 'package:flutter/foundation.dart'; 
 import 'package:jrrplayerapp/services/audio_player_service.dart';
 import 'dart:async';
+import 'package:just_audio/just_audio.dart';
 
 class AudioPlayerHandler extends BaseAudioHandler {
   final AudioPlayerService audioPlayerService;
